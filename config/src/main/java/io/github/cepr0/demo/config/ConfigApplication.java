78d169f4a2e70b2143454904b8dea79a85af83fe
Sergei Poznanski <cepr0@ukr.net>
2019-07-20 14:53:23 +0300

    Simplify Config app

--- config/src/main/java/io/github/cepr0/demo/config/ConfigApplication.java
+++ config/src/main/java/io/github/cepr0/demo/config/ConfigApplication.java
@@ -1,11 +1,9 @@
 package io.github.cepr0.demo.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @EnableBinding(Routes.class)
@@ -13,13 +11,8 @@ import org.springframework.context.annotation.Bean;
 public class ConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
-	}
-
-	@Bean
-	ApplicationRunner runner() {
-		return args -> {
-			System.exit(0);
-		};
+		SpringApplication
+				.run(ConfigApplication.class, args)
+				.close();
 	}
 }
